716121d9afd1f20971e8cafbec225a0869a1e84d
fanxq <fan_xq@qq.com>
2022-05-17 22:26:32 +0800

    1. add common post api

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xqvan/gu
+
+go 1.18
---
+++ http_common_request.go
@@ -0,0 +1,38 @@
+package gu
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func CommonPostJson(url string, headers map[string]string, body map[string]interface{}) ([]byte, error) {
+	payloadByte, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+
+	payload := bytes.NewReader(payloadByte)
+
+	req, err := http.NewRequest(http.MethodPost, url, payload)
+	if err != nil {
+		return nil, err
+
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	for s, a := range headers {
+		req.Header.Add(s, a)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	resBody, err := io.ReadAll(res.Body)
+
+	return resBody, err
+}
